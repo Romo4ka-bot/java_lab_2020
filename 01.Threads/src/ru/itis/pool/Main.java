9a20ecd24f8afe85623eead00d16a31d0adbf959
Roman Leontev <974078rl@gmail.com>
2020-09-14 11:18:55 +0300

    q

---
+++ 01.Threads/src/ru/itis/pool/Main.java
@@ -0,0 +1,41 @@
+package ru.itis.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) {
+//        ExecutorService executorService = Executors.newFixedThreadPool(2);
+//
+        ThreadPool threadPool = ThreadPool.newPool(2);
+        Runnable task1 = () -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println(Thread.currentThread().getName() + " A");
+            }
+        };
+
+        Runnable task2 = () -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println(Thread.currentThread().getName() + " B");
+            }
+        };
+
+        Runnable task3 = () -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println(Thread.currentThread().getName() + " C");
+            }
+        };
+//
+//        executorService.submit(task1);
+//        executorService.submit(task2);
+//        executorService.submit(task3);
+//    }
+
+
+        threadPool.submit(task1);
+        threadPool.submit(task2);
+        threadPool.submit(task3);
+
+    }
+}
+
