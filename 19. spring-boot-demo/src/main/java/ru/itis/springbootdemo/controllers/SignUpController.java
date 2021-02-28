fb951faad9f4493a79884218324e48858a44d9df
Roman Leontev <974078rl@gmail.com>
2021-02-28 14:17:26 +0300

    add validation and localization

--- 19. spring-boot-demo/src/main/java/ru/itis/springbootdemo/controllers/SignUpController.java
+++ 19. spring-boot-demo/src/main/java/ru/itis/springbootdemo/controllers/SignUpController.java
@@ -2,15 +2,26 @@ package ru.itis.springbootdemo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.itis.springbootdemo.dto.UserForm;
 import ru.itis.springbootdemo.services.SignUpService;
 
+import javax.validation.Valid;
+import java.util.Objects;
+
 @Controller
 public class SignUpController {
+
     @Autowired
     private SignUpService signUpService;
+
+    @GetMapping("/success")
+    public String getSuccessPage() {
+        return "success_signup";
+    }
 
     @GetMapping("/signUp")
     public String getSignUpPage() {
@@ -18,8 +29,18 @@ public class SignUpController {
     }
 
     @PostMapping("/signUp")
-    public String signUp(UserForm form) {
+    public String signUp(@Valid UserForm form, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().stream().anyMatch(error -> {
+                if (Objects.requireNonNull(error.getCodes())[0].equals("userForm.ValidNames")) {
+                    model.addAttribute("namesErrorMessage", error.getDefaultMessage());
+                }
+                return true;
+            });
+            model.addAttribute("userForm", form);
+            return "sign_up_page";
+        }
         signUpService.signUp(form);
-        return "redirect:/users";
+        return "redirect:/success";
     }
 }
